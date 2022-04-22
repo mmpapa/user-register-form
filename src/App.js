ca081045547dfbd78e76b8fbbbffe13a0a0c2d4a
mmpapa <mmpapa9804@gmail.com>
2022-04-22 15:13:29 +0900

    Auth入れた

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import {Authenticator} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
 import UerProfileRegisterPage  from "./pages/UerProfileRegisterPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/profile" element={<UserProfilePage />} />
-        <Route path="/" element={<UerProfileRegisterPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Authenticator socialProviders={['amazon', 'apple', 'facebook', 'google']}> 
+    {({signOut, user}) =>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/profile" element={<UserProfilePage />} />
+          <Route path="/" element={<UerProfileRegisterPage />} />
+        </Routes>
+        <button onClick={signOut}>Sign out</button>
+      </BrowserRouter>
+      
+    }
+    </Authenticator>
   );
 }
 
